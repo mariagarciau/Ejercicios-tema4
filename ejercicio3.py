4a6cb224335ea03d5b7112f82ff7c6b1661efeec
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-22 16:47:32 +0100

    no funciona no se por que

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,14 @@
 12 años y más: cadete
 Escriba el programa Python asociado.
 """
+edad = int(input("Introduzca su edad en años: "))
+if edad>6:
+    print("No puede jugar en ninguna categoria")
+elif edad == 6 or edad == 7:
+    print("Su categoria es benjamin")
+elif edad == 8 or edad== 9:
+    print("Su categoria es alevin")
+elif edad == 10 or edad== 11:
+    print("Su categoria es infantil")
+elif edad >=12:
+    print("Su categoria es cadete")
