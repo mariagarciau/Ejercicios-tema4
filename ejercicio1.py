ad8932a9daaedc77c5b08333490b384a8e7e0431
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-22 16:16:47 +0100

    funciona

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,10 @@
 """Escriba, usando comparaciones, un algoritmo que muestre el estado del agua
 (hielo, líquido, vapor) en función de su temperatura.
 """
+temperatura = 101
+if temperatura<0:
+    print("El agua esta en estado sólido")
+elif temperatura>0 and temperatura<100:
+    print("El agua esta en estado líquido")
+else:
+    print("El agua esta en estado gaseoso")
