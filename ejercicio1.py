87ea2086ecdd74e6ff047f6065c32dcc0a653047
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-22 16:10:19 +0100

    enunciados

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+"""Escriba, usando comparaciones, un algoritmo que muestre el estado del agua
+(hielo, líquido, vapor) en función de su temperatura.
+"""
---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+"""Escriba el mismo algoritmo, pero utilizando dos variables buleanas para comprobar el estado del agua,
+sin comparaciones en los SI.
+"""
