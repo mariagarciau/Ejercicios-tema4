16342b2b650018befcb7e227216dc2c2852cdebe
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-22 16:30:12 +0100

    primera prueba

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,20 @@
 """Escriba el mismo algoritmo, pero utilizando dos variables buleanas para comprobar el estado del agua,
 sin comparaciones en los SI.
 """
+si = True
+no = False
+def es_gaseoso(temperatura):
+    if temperatura>100:
+        return si
+    else:
+        return no
+def es_liquido(temperatura):
+    if temperatura>=0 and temperatura<=100:
+        return si
+    else:
+        return no
+def es_solido(temperatura):
+    if temperatura<0:
+        return si
+    else:
+        return no
